7e7e6dcac470d6999596b1401f470903ee442612
Pablo Gros <rob.r.pete@gmail.com>
2020-03-03 16:26:43 -0500

    update reset script, on reset it resets the scene and displays a message

--- Assets/Scripts/ResetOnFloorCollison.cs
+++ Assets/Scripts/ResetOnFloorCollison.cs
@@ -11,13 +11,32 @@ public class ResetOnFloorCollison : MonoBehaviour
     [SerializeField]
     string p2;
 
+    public GameObject uiObject;
+
     //score for each player
     int p1score;
     int p2score;
+
+    private void Start() 
+    {
+        uiObject.SetActive(false);
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.collider.tag ==  p1 || collision.collider.tag == p2) 
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        if(collision.gameObject.tag == p1 || collision.gameObject.tag == p2)
+        {
+            uiObject.SetActive(true);
+            StartCoroutine("WaitForSec");
+        }
+    }
+
+    IEnumerable WaitForSec()
+    {
+        yield return new WaitForSeconds(10);
+        Destroy(uiObject);
     }
 }
