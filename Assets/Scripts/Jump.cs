ef2eef676319c4996a971099bc6648e47508f3be
Pablo Gros <rob.r.pete@gmail.com>
2020-03-03 15:32:27 -0500

    fixed unity code not working with vs problem, changed default editor to vsCode and installed three packages that are useful with unity in vsCode, also added reset scene script and attached to redCude(floor) which resets the scene when an object with player tag hits it

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+    public bool ballIsOnGround = true;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetButtonDown("Jump") && ballIsOnGround)
+        {
+            rigidbody.AddForce(new Vector3(0, 4, 0), ForceMode.Impulse);
+            ballIsOnGround = false;
+        }
+    }
+
+    private void onCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            ballIsOnGround = true;
+        }
+    }
+}
