72da2d185558c515e6d82e9f0368ad26dc10a7bc
Pablo Gros <rob.r.pete@gmail.com>
2020-03-03 15:37:58 -0500

    update player two to have own tag and added second variable in reset script so that if tag player or tag player2 hit floor scene resets

--- Assets/Scripts/ResetOnFloorCollison.cs
+++ Assets/Scripts/ResetOnFloorCollison.cs
@@ -6,11 +6,14 @@ using UnityEngine.SceneManagement;
 public class ResetOnFloorCollison : MonoBehaviour
 {
     [SerializeField]
-    string strTag;
+    string p1;
+
+    [SerializeField]
+    string p2;
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.collider.tag == strTag)
+        if(collision.collider.tag ==  p1 || collision.collider.tag == p2) 
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
