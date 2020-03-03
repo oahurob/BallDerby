2d3f532573c4438ec5e616400cb8b8fb539d77a6
Pablo Gros <rob.r.pete@gmail.com>
2020-03-03 12:06:34 -0500

    imported assets to have a ramp, added some structures that are static to the arena, also added a second player(yellow and black ball) with its own script for movement

---
+++ Assets/Scripts/PlayerTwoVelocity.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTwoVelocity : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 v3;
+
+    [SerializeField]
+    KeyCode keyPositive;
+
+    [SerializeField]
+    KeyCode keyNegative;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(keyPositive))
+            GetComponent<Rigidbody>().velocity += v3;
+        if (Input.GetKey(keyNegative))
+            GetComponent<Rigidbody>().velocity -= v3;
+    }
+}
