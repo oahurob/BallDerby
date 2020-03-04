dc8924b08d2197e63280fc5347f23a4c93f053dc
Pablo Gros <rob.r.pete@gmail.com>
2020-03-04 15:55:15 -0500

    Add mainMenu with functionality for play and quit button

---
+++ Assets/Scripts/DisplayWinner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayWinner : MonoBehaviour
+{
+    public GameObject uiWinner1;
+    public GameObject uiWinner2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiWinner1.SetActive(false);
+        uiWinner2.SetActive(false);
+    }
+
+    void ShowWhoWon()
+    {
+        if(KeepScoreP1.Scorep1 == 3)
+        {
+            uiWinner1.SetActive(true);
+        }
+        
+        if(KeepScoreP2.Scorep2 == 3)
+        {
+            uiWinner2.SetActive(true);
+        }
+    }
+
+}
